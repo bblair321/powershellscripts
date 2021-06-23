060ab1df92d755771c4db422fbb59852e0ff8153
bblair321 <81390380+bblair321@users.noreply.github.com>
2021-06-23 15:10:25 -0700

    Add files via upload

---
+++ valheimserverbackup.ps1
@@ -0,0 +1,5 @@
+﻿Stop-Process -Name "valheim_server"
+cd C:\
+Copy-Item -Path "C:\valheimserver*" -Destination "\\192.168.0.4\Mod Depository\ValheimMods\ServerBackups\$(get-date -f "yyyy-MM-dd-hh-mm")" -Recurse
+cd C:\valheimserver
+Start-Process "start_headless_server.bat"
